fe874291c713eff4ad5f5d9849744099cd623623
Ai-Mei <purcia.princessmae296@gmail.com>
2023-07-01 18:34:33 +0800

    Psuedocode

--- Fan.py
+++ Fan.py
@@ -1,1 +1,8 @@
-
+# Create a Fan class
+# Assign 3 variables slow, medium, and fast
+# Constructor that creates a fan with the specified speed (default Slow), radius (default 5), color (default blue), and on (default False).
+# Private int data field named speed that specifies the speed of the fan.
+# Private bool data field named on that specifies whether the fan is on (the default is False).
+# Private float data field named radius that specifies the radius of the fan.
+# Private string data field named color that specifies the color of the fan.
+# The accessor(getters)  and mutator(setters)  methods for all four data fields.
--- TestFan.py
+++ TestFan.py
@@ -1,1 +1,3 @@
-
+# 1st Object = Maximum Speed, radius 10, color Yellow, and turn it on.
+# 2nd Object = Medium speed, radius 5, color blue, and turn it off.
+# Display each object’s speed, radius, color, and on properties.
