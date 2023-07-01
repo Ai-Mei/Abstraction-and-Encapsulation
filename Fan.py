ecc0aceb8218b8ddeb4716ede930d67d08a7ff08
Ai-Mei <purcia.princessmae296@gmail.com>
2023-07-01 19:18:47 +0800

    get and set methods

--- Fan.py
+++ Fan.py
@@ -4,14 +4,40 @@ class Fan:
     slow = 1
     medium = 2
     fast = 3
+
 # Constructor that creates a fan with the specified speed (default Slow), radius (default 5), color (default blue), and on (default False).
     def __init__(self, speed = 1, radius = 5, color = "Blue", on = False):
         self.__speed = speed
         self.__on = on
         self.__radius = radius
         self.__color = color
-# Private int data field named speed that specifies the speed of the fan.
-# Private bool data field named on that specifies whether the fan is on (the default is False).
-# Private float data field named radius that specifies the radius of the fan.
-# Private string data field named color that specifies the color of the fan.
+
 # The accessor(getters)  and mutator(setters)  methods for all four data fields.
+    def get_speed(self):
+        if self.__speed == Fan.slow:
+            return "Slow"
+        elif self.__speed == Fan.medium:
+            return "Medium"
+        elif self.__speed == Fan.fast:
+            return "Fast"
+        
+    def set_speed(self, speed):
+        self.__speed = speed
+
+    def get_on(self):
+        return self.__on
+    
+    def set_on(self, on):
+        self.__on = on
+
+    def get_radius(self):
+        return self.__radius
+    
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def get_color(self):
+        return self.__color
+    
+    def set_color(self, color):
+        self.__color = color
